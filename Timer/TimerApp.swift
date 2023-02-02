5b86897fdc9584aa2fa79127ca9bd37209634d23
Syu <mirzamrion@gmail.com>
2023-02-02 13:10:28 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 村瀬嵩 on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
